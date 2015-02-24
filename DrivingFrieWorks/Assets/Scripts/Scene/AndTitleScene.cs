7237d72362462ea11bc3795e74cc192a4052a331
tetsujp <kasei84@gmail.com>
2015-02-25 00:48:00 +0900

    2

---
+++ DrivingFrieWorks/Assets/Scripts/Scene/AndTitleScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndTitleScene : BasicScene{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Initialize();
+    }
+    public override void Initialize()
+    {
+
+    }
+    public override void SceneFinalize()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Decide"))
+        {
+            //曲選択画面へ
+
+            ChangeScene(SceneName.Select);
+        }
+    }
+}
